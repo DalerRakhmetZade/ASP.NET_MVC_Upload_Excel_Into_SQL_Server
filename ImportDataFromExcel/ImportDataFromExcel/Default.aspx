1d69c0abd388e79b4dbddff2b473ad60deab0e51
Daler Rakhmet-Zade <daler4ik@gmail.com>
2018-01-17 13:42:01 -0800

    Add files via upload

---
+++ ImportDataFromExcel/ImportDataFromExcel/Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ImportDataFromExcel._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h3>Prospect Client List</h3>
+    <hr />
+    <div>
+        
+        <div>
+            <table>
+                <tr>
+                    <td>Select File: </td>
+                    <td>
+                        <asp:FileUpload ID="FileUpload1" runat="server" CssClass="btn btn-primary" /></td>
+                    <td>
+                        <asp:Button ID="btnImportFromCSV" runat="server" Text="Import" CssClass="btn btn-primary" OnClick="btnImportFromCSV_Click" />
+                    &nbsp;&nbsp; 
+                    </td>
+                </tr>
+            </table>
+            <div>
+                <em>
+                <asp:Label ID="lblMessage" runat="server" style="color: #33CC33; font-size: small" />
+                        </em>
+                <br />
+                <br />
+                <br />
+                <asp:GridView ID="gvData" runat="server" AutoGenerateColumns="false" Height="170px" OnSelectedIndexChanged="gvData_SelectedIndexChanged" Width="592px">
+                    <EmptyDataTemplate>
+                        <div style="padding: 10px;">
+                            <h4>Please import Excel spread sheet</h4>
+                        </div>
+                    </EmptyDataTemplate>
+                    <Columns>
+                        <asp:BoundField HeaderText="Client ID" HeaderStyle-BackColor="Black" HeaderStyle-ForeColor="AntiqueWhite" HeaderStyle-Font-Size="Medium" DataField="ClientID" />
+                        <asp:BoundField HeaderText="First Name" HeaderStyle-BackColor="Black" HeaderStyle-ForeColor="AntiqueWhite" HeaderStyle-Font-Size="Medium" DataField="First_Name" />
+                        <asp:BoundField HeaderText="Last Name" HeaderStyle-BackColor="Black" HeaderStyle-ForeColor="AntiqueWhite" HeaderStyle-Font-Size="Medium" DataField="Last_Name" />
+                        <asp:BoundField HeaderText="Employer" HeaderStyle-BackColor="Black" HeaderStyle-ForeColor="AntiqueWhite" HeaderStyle-Font-Size="Medium" DataField="Employer" />
+                        <asp:BoundField HeaderText="Title" HeaderStyle-BackColor="Black" HeaderStyle-ForeColor="AntiqueWhite" HeaderStyle-Font-Size="Medium" DataField="Title" />
+                        <asp:BoundField HeaderText="Phone Number" HeaderStyle-BackColor="Black" HeaderStyle-ForeColor="AntiqueWhite" HeaderStyle-Font-Size="Medium" DataField="Phone_Number" />
+                        <asp:BoundField HeaderText="Zip" HeaderStyle-BackColor="Black" HeaderStyle-ForeColor="AntiqueWhite" HeaderStyle-Font-Size="Medium" DataField="Zip" />
+                    </Columns>
+                </asp:GridView>
+                <br />
+
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
+
---
+++ ImportDataFromExcel/ImportDataFromExcel/Default.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ImportDataFromExcel
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                populateDatabaseData();
+                
+            }
+        }
+        private void populateDatabaseData()
+        {
+            using (ClientDataEntities1 dc = new ClientDataEntities1())
+            {
+                gvData.DataSource = dc.ClientMasters.ToList();
+                gvData.DataBind();
+            }
+        }
+        protected void btnImportFromCSV_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.PostedFile.ContentType == "application/vnd.ms-excel" ||
+                FileUpload1.PostedFile.ContentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            {
+
+                try
+                {
+                    string fileName = System.IO.Path.Combine(Server.MapPath("~/UploadDocuments"), Guid.NewGuid().ToString() + Path.GetExtension(FileUpload1.PostedFile.FileName));
+                    FileUpload1.PostedFile.SaveAs(fileName);
+
+                    string conString = "";
+                    string ext = Path.GetExtension(FileUpload1.PostedFile.FileName);
+                    if (ext.ToLower() == ".xls")
+                    {
+                        conString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
+                    }
+                    else if (ext.ToLower() == ".xlsx")
+                    {
+                        conString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+                    }
+
+                    string query = "Select [ClientID], [First_Name], [Last_Name], [Employer], [Title], [Phone_Number], [Zip] from [ClientList$]";
+
+                    OleDbConnection con = new OleDbConnection(conString);
+                    if (con.State == System.Data.ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    OleDbCommand cmd = new OleDbCommand(query, con);
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    da.Dispose();
+                    con.Close();
+                    con.Dispose();
+
+                    // Update database data
+                    using (ClientDataEntities1 dc = new ClientDataEntities1())
+                    {
+                        foreach (DataRow dr in ds.Tables[0].Rows)
+                        {
+                            string empID = dr["ClientID"].ToString();
+                            var v = dc.ClientMasters.Where(a => a.ClientID.Equals(empID)).FirstOrDefault();
+                            if (v != null)
+                            {
+                                v.First_Name = dr["First_Name"].ToString();
+                                v.Last_Name = dr["Last_Name"].ToString();
+                                v.Employer = dr["Employer"].ToString();
+                                v.Title = dr["Title"].ToString();
+                                v.Phone_Number = dr["Phone_Number"].ToString();
+                                v.Zip = dr["Zip"].ToString();
+
+                            }
+                            else
+                            {
+                                dc.ClientMasters.Add(new ClientMaster
+                                {
+                                    ClientID = dr["ClientID"].ToString(),
+                                    First_Name = dr["First_Name"].ToString(),
+                                    Last_Name = dr["Last_Name"].ToString(),
+                                    Employer = dr["Employer"].ToString(),
+                                    Title = dr["Title"].ToString(),
+                                    Phone_Number = dr["Phone_Number"].ToString(),
+                                    Zip = dr["Zip"].ToString(),
+                                });
+                            }
+                        }
+
+                        dc.SaveChanges();
+
+                        // populate updated data 
+                        populateDatabaseData();
+                        lblMessage.Text = "Excel file has been imported Successfully!";
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        protected void gvData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
